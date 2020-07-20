55214433038c9b364fc8c636fc8bb751e2a2e89d
Ciara Picou <ciarapicou@att.net>
2020-07-20 22:17:25 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -6,8 +6,8 @@ def find_element_index(array, value_to_find)
 count = 0
  while count < array.size do
    if value_to_find == array.count
-     p count 
+     p count
+   end
    count += 1
- end
  end
    
