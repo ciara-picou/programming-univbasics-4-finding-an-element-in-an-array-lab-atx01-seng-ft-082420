81801262dbf080a6e8d019cbeacb185ac7d0c1b0
Ciara Picou <ciarapicou@att.net>
2020-07-20 22:41:56 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,14 @@
-def find_element_index(array, value_to_find)
- array.include?(value_to_find)? array.index(value_to_find) : nil
+# def find_element_index(array, value_to_find)
+# array.include?(value_to_find)? array.index(value_to_find) : nil
+# end
+
+
+   def find_element_index(array, value_to_find)
+     count = 0
+     while count < array.length
+     if array[count] == value_to_find
+       return count
+      end
+  count += 1
 end
+end 
